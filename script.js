36d2f428b062647ab1f56eb120df4f08a0fc3ee1
berry <kathborowy@gmail.com>
2022-06-07 11:48:08 +0200

    function plays a sound when a certain button is pressed

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', (e) => {
+    //let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (audio === null) return;
+    
+    audio.play();
     
 });
