630a2e4de4236f000f929a35045f87037e2119fd
berry <kathborowy@gmail.com>
2022-06-07 15:41:43 +0200

    added event listener that removes 'playing' class

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.addEventListener('keydown', (e) => {
-    //let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (audio === null) return;
     
+    key.classList.toggle('playing')
     audio.play();
     audio.currentTime = 0;
-});
+    console.log(e.propertyName)
+  });
+  
+  window.addEventListener('keyup', (e) => {
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.remove('playing')
+  })
+  
+  
